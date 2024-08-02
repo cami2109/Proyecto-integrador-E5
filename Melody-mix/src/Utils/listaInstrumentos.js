c22143d7c05bc65bedcecee75e191d9000cdcf86
Santiago Navarro <santinavarro2003@gmail.com>
2024-08-02 14:32:13 -0300

    Coreccion objeto mock

--- Melody-mix/src/Utils/listaInstrumentos.js
+++ Melody-mix/src/Utils/listaInstrumentos.js
@@ -1,4 +1,4 @@
-let instrumentos = [
+export const instrumentos = [
     {
         nombre: "Piano",
         precio: "1000",
@@ -25,3 +25,4 @@ let instrumentos = [
         imagen: "https://imgs.search.brave.com/iXhvNpIGqjdWSeAi8caZTegDsDxsGe5uKVbYQTNkvkw/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9hdWRp/b211c2ljYWNsLnZ0/ZXhhc3NldHMuY29t/L2Fzc2V0cy92dGV4/LmZpbGUtbWFuYWdl/ci1ncmFwaHFsL2lt/YWdlcy9kZjc0MTZm/ZS0yNTBmLTQzNGQt/YWM1Yi04NjBjOTBl/YTQ1YzZfX182NTY4/NDE5MTM1MjQ3NDZh/M2U0NzZiOTI0ZDhl/NzJiOC5qcGc"
     }
 ]
+
