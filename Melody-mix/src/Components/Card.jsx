5d1c31476351cbf5271c321e7445560f199a5f90
Santiago Navarro <santinavarro2003@gmail.com>
2024-08-12 22:47:03 -0300

    asd

--- Melody-mix/src/Components/Card.jsx
+++ Melody-mix/src/Components/Card.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
-const Card = () => {
+const Card = ({ info }) => {
+  const { nombre, imagen, precio, id } = info;
+
   return (
-    <div>
-        
+    <div className="card">
+      <Link to={"/details/" + id}>
+        <img src={imagen} width={200} />
+        <div>
+          <h2>{nombre}</h2>
+          <h4>{precio}</h4>
+          <h4>ver mas...</h4>
+        </div>
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
---
+++ Melody-mix/src/Components/Recomendaciones.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import "../App.css"
+import { instrumentos } from "../Utils/listaInstrumentos"
+import Card from './Card'
+
+const Recomendaciones = () => {
+  return (
+    <section className="section">
+        <div>
+          <h2>Recomendaciones</h2>
+        </div>
+        <div className="section-bottom">          
+          { instrumentos.map((item) => {
+            return (                
+                <Card key={item.id} info={item}/>
+            )
+          })}
+        </div>
+      </section>
+  )
+}
+
+export default Recomendaciones
