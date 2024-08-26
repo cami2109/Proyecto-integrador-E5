bb1004247c68ffb30f3cbda57bee463ef1b4382f
jadls <jorge.delosantos@gmail.com>
2024-08-26 18:37:31 -0300

    tamaño imágenes y responsive

--- Melody-mix/src/Components/Header.jsx
+++ Melody-mix/src/Components/Header.jsx
@@ -1,31 +1,43 @@
-import React from "react"
+import React from "react";
 import { Link } from "react-router-dom";
 import { useUserContext } from "../Context/Context";
 
-
 const Header = () => {
-  const {state} = useUserContext()
+  const { state } = useUserContext();
 
   return (
-    <div className="header" >
+    <div className="header">
       <div className="header-left">
         <div className="logo">
-        <Link to="/"><img src='/Img/logo.png' alt="LOGO" width={140}/></Link>
-        {/* <Link to="/"><FontAwesomeIcon icon="fa-solid fa-bars" fade /></Link> */}
+          <Link to="/">
+            <img src="/Img/logo.png" alt="LOGO" width={140} />
+          </Link>
         </div>
         <div>Tu viaje musical a un click</div>
       </div>
       <div className="header-right">
         <nav className="nav">
-          {state.user.nombre ? <Link to={"/perfil"}><h4>{state.user.nombre + ' ' + state.user.apellido}</h4></Link>: <><Link to="/login"><h4>Login</h4></Link><Link to="/crearcuenta"><h4>Crear Cuenta</h4></Link></>}
-          {state.admin && <h4>Modo Admin</h4>}
+          {state.user.name ? (
+            <Link to={"/perfil"}>
+              <h4>{state.user.name}</h4>
+            </Link>
+          ) : (
+            <>
+              <Link to="/login">
+                <h4>Login</h4>
+              </Link>
+              <Link to="/crearcuenta">
+                <h4>Crear Cuenta</h4>
+              </Link>
+            </>
+          )}
         </nav>
-        <div className="hamb">
-          <img src="Img/hamb.png" alt="hamburguesa" width={30}/>
+        <div class="hamb">
+          <img src="Img/hamb.png" alt="hamburguesa" width={30} />
         </div>
       </div>
     </div>
   );
 };
 
-export default Header
+export default Header;
