d1dee9210a29a5aec9b58c8addd3d08b41767f34
Ignacio <ignaciosigal04@gmail.com>
2024-08-27 20:45:20 -0300

    aa

---
+++ melodyBackend/melodyBackend/src/main/java/com/melodymix/controlador/UsuarioControlador.java
@@ -0,0 +1,66 @@
+package com.melodymix.controlador;
+
+import com.melodymix.entidad.Usuario;
+import com.melodymix.servicio.UsuarioServicioImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+
+@RestController
+// mapeo la url para que todos los postmapping que se hagan en este controlador,
+// tengan que tener "/usuario" antes, osea /usuario/registro o /usuario/login , etc
+@RequestMapping("/usuario")
+
+public class UsuarioControlador {
+    // inyectamos la implementacion UsuarioServiceImpl
+    private UsuarioServicioImpl usuarioServiceImpl;
+
+    // contrasena admin
+    @Value("${admin.contrasena}")
+    private String adminContrasena;
+
+    @Autowired
+    public UsuarioControlador(UsuarioServicioImpl usuarioServiceImpl) {
+        this.usuarioServiceImpl = usuarioServiceImpl;
+    }
+
+    @PostMapping("/registro")
+    public ResponseEntity<Usuario> registrar(@RequestBody Usuario usuario) {
+        Usuario nuevoUsuario = usuarioServiceImpl.registrar(usuario);
+        return ResponseEntity.ok(nuevoUsuario);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody Usuario usuario) {
+        Usuario usuarioRegistrado = usuarioServiceImpl.buscarPorEmail(usuario.getEmail());
+        if (usuarioRegistrado != null && usuarioServiceImpl.getPasswordEncoder().matches(usuario.getContrasena(), usuarioRegistrado.getContrasena())) {
+            boolean isAdmin = usuario.getContrasena().equals(adminContrasena);
+
+            Authentication authentication = new UsernamePasswordAuthenticationToken(
+                    usuario.getEmail(),
+                    usuario.getContrasena(),
+                    Collections.singletonList(new SimpleGrantedAuthority(isAdmin ? "ROL_ADMIN" : "ROL?_USUARIO"))
+            );
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            return ResponseEntity.ok(isAdmin ? "Login exitoso como ADMIN" : "Login exitoso como USUARIO");
+        }
+
+        return ResponseEntity.status(401).body("Login fallido");
+    }
+
+
+
+
+}
---
+++ melodyBackend/melodyBackend/src/main/java/com/melodymix/servicio/UsuarioServicioImpl.java
@@ -0,0 +1,85 @@
+package com.melodymix.servicio;
+
+import com.melodymix.entidad.Usuario;
+import com.melodymix.repo.IUsuarioRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.*;
+
+@Service
+public class UsuarioServicioImpl implements IUsuarioServicio, UserDetailsService {
+
+    private final IUsuarioRepositorio usuarioRepositorio;
+    private final PasswordEncoder passwordEncoder;
+
+    @Value("${admin.contrasena}")
+    private String adminContrasena;
+
+
+
+    // Autowired sirve para inyectar una instancia de IUsuarioRepositorio en esta clase
+    // osea, cuando se crea un UsuarioServiceImpl, spring se encarga de darle una instancia
+    // adecuada de IUsuarioRepositorio
+    // esto permite que UsuarioServiceImpl use el repositorio para acceder a la base de datos
+    @Autowired
+    public UsuarioServicioImpl(IUsuarioRepositorio usuarioRepositorio, PasswordEncoder passwordEncoder) {
+        this.usuarioRepositorio = usuarioRepositorio;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public PasswordEncoder getPasswordEncoder() {
+        return passwordEncoder;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Usuario usuario = buscarPorEmail(email);
+        if (usuario == null) {
+            throw new UsernameNotFoundException("Usuario no encontrado con el email: " + email);
+        }
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(usuario.getEmail())
+                .password(usuario.getContrasena())
+                .roles(usuario.isAdmin() ? "ADMIN" :"USER")
+                .build();
+    }
+
+    @Override
+    public Usuario registrar(Usuario usuario) {
+//        if (usuario.getContrsenaAdmin() != null && usuario.getContrsenaAdmin().equals(admin)) esto quedo viejo del admin en registro, ahora es para login
+        usuario.setContrasena(passwordEncoder.encode(usuario.getContrasena()));
+        return usuarioRepositorio.save(usuario);
+    }
+
+    @Override
+    public List<Usuario> listarTodos() {
+        return usuarioRepositorio.findAll();
+    }
+
+    @Override
+    public Usuario buscarPorId(Long id) {
+        return usuarioRepositorio.findById(id).orElse(null);
+    }
+
+    @Override
+    public void eliminar(Long id) {
+        usuarioRepositorio.deleteById(id);
+    }
+
+    @Override
+    public void actualizar(Usuario usuario) {
+        // Implementar la lógica de actualización si es necesario
+        usuarioRepositorio.save(usuario);
+    }
+
+    @Override
+    public Usuario buscarPorEmail(String email) {
+        return usuarioRepositorio.findByEmail(email).orElse(null);
+    }
+}
