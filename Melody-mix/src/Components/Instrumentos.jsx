5e602838a838a42f185050eaf000dba9be09c339
jadls <jorge.delosantos@gmail.com>
2024-09-02 15:37:29 -0300

    imagenes recortadas para organizar las vistas. descartar instrumentos.jsx

--- Melody-mix/src/Components/Instrumentos.jsx
+++ Melody-mix/src/Components/Instrumentos.jsx
@@ -1,34 +1,31 @@
-import React from 'react'
-import "../App.css"
+import React from "react";
+import "../App.css";
+import { useUserContext } from "../Context/Context";
+import { categorias } from "../Utils/listaCategorias";
 
 const Instrumentos = () => {
+  const { state } = useUserContext();
+  const instrumentos = state.products;
+
   return (
     <section className="section">
-        <h2>Instrumentos por tipo</h2>
-        <div className="section-mid">
-          <div key={1} className="card">
-            {/* <h3>Card {1}</h3>
-            <p> card {1}</p> */}
-            <img src='/Img/instrumento varios.PNG' alt="LOGO" style={{width: "100%"}}/>
-          </div>
-          <div key={2} className="card">
-            {/* <h3>Card {2}</h3>
-            <p> card {2}</p> */}
-            <img src='/Img/instrumento percucion.PNG' alt="LOGO" style={{width: "100%"}}/>
-          </div>
-          <div key={3} className="card">
-            {/* <h3>Card {3}</h3>
-            <p> card {3}</p> */}
-            <img src='/Img/instrumento de viento.PNG' alt="LOGO" style={{width: "100%"}}/>
-          </div>
-          <div key={4} className="card">
-            {/* <h3>Card {4}</h3>
-            <p> card {4}</p> */}
-            <img src='/Img/Instrumento de cuerda.PNG' alt="LOGO" style={{width: "100%"}}/>
-          </div>
-        </div>
-      </section>
-  )
-}
+      <h2>Instrumentos por tipo</h2>
+      <div className="section-mid">
+        {categorias.map((categoria) => {
+          console.log(categoria);
+          return (
+            <div key={categoria.id} className="card">
+              <img
+                src={categoria.imagen}
+                alt="LOGO"
+                style={{ width: "100%" }}
+              />
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
 
-export default Instrumentos
+export default Instrumentos;
---
+++ Melody-mix/src/Utils/listaCategorias.js
@@ -0,0 +1,23 @@
+export const categorias = [
+    {
+        id: 1,
+        categoria: "viento",
+        imagen: "/Img/recortada_CategCuerda.jpg"
+    },
+    {
+        id: 2,
+        categoria: "cuerda",        
+        imagen: "/Img/recortada_CategViento.jpg"
+    },
+    {
+        id: 3,
+        categoria: "percusion",
+        imagen: "/Img/recortada_CategPercusion.jpg"
+    },
+    {
+        id: 4,
+        categoria: "varios",
+        imagen: "/Img/recortada_CategVarios.jpg"
+    }
+
+]
