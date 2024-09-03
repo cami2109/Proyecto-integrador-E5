1cdab11babc9c8c84fc199e520fba7b33635a718
Ignacio <ignaciosigal04@gmail.com>
2024-09-03 17:20:34 -0300

    imagenurl

--- melodyBackend/melodyBackend/src/main/java/com/melodymix/controlador/InstrumentoControlador.java
+++ melodyBackend/melodyBackend/src/main/java/com/melodymix/controlador/InstrumentoControlador.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 
 @RestController
-@RequestMapping("instrumento")
+@RequestMapping("/instrumento")
 public class InstrumentoControlador {
     @Autowired
     private InstrumentoServicioImpl instrumentoServicioImpl;
